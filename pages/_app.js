b4ea0f85795daeaf48de8ce015383e6dc300e9ff
Danylo Diachenko <danildiachenko23@gmail.com>
2023-07-09 22:48:08 +0300

    init

---
+++ pages/_app.js
@@ -0,0 +1,23 @@
+import { useRouter } from 'next/router';
+import { AnimatePresence, motion } from 'framer-motion';
+
+import '../styles/globals.css';
+import Layout from '../components/Layout';
+import Transition from "../components/Transition";
+
+function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  return (
+    <Layout>
+      <AnimatePresence mode='wait'>
+        <motion.div key={router.route} className='h-full'>
+          <Transition />
+          <Component {...pageProps} />
+        </motion.div>
+      </AnimatePresence>
+    </Layout>
+  );
+}
+
+export default MyApp;
