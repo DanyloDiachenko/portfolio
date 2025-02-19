e777627f4c253b0bbe282ee0ea936af730fd4f25
Danylo <107757498+DanyloDiachenko@users.noreply.github.com>
2025-02-19 23:00:31 +0200

    Update index.tsx

--- pages/work/index.tsx
+++ pages/work/index.tsx
@@ -9,6 +9,7 @@ const Work = (): JSX.Element => {
     return (
         <div className="h-full bg-primary/30 py-36 flex items-center">
             <Circles />
+            
             <div className="container mx-auto max-xl:overflow-y-auto max-xl:h-full">
                 <div className="flex flex-col xl:flex-row gap-x-8">
                     <div className="text-center flex xl:w-[30vw] flex-col lg:text-left mb-4 xl:mb-0">
@@ -20,7 +21,6 @@ const Work = (): JSX.Element => {
                             className="h2 xl:mt-12"
                         >
                             My work
-                            
                             <span className="text-accent"> .</span>
                         </motion.h1>
                         <motion.p
