e6225282526ee203c27ee39b03b91cc3367c980a
Danylo <107757498+DanyloDiachenko@users.noreply.github.com>
2025-02-19 23:00:36 +0200

    Update index.tsx

--- pages/work/index.tsx
+++ pages/work/index.tsx
@@ -9,7 +9,6 @@ const Work = (): JSX.Element => {
     return (
         <div className="h-full bg-primary/30 py-36 flex items-center">
             <Circles />
-            
             <div className="container mx-auto max-xl:overflow-y-auto max-xl:h-full">
                 <div className="flex flex-col xl:flex-row gap-x-8">
                     <div className="text-center flex xl:w-[30vw] flex-col lg:text-left mb-4 xl:mb-0">
